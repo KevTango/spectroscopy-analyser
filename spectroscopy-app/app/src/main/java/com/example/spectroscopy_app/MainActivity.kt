251ffb7b2761fa9c87e72148fe1a2ea7f1d64887
kevin <kevin.tang2648@gmail.com>
2021-05-09 01:43:03 +1200

    add http communication

--- spectroscopy-app/app/src/main/java/com/example/spectroscopy_app/MainActivity.kt
+++ spectroscopy-app/app/src/main/java/com/example/spectroscopy_app/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.spectroscopy_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to button
+        val btn_click_me = findViewById(R.id.connect_button) as Button
+        // set on-click listener
+        btn_click_me.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@MainActivity, "Connecting...", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
--- spectroscopy-esp8266/spectroscopy-esp8266.ino
+++ spectroscopy-esp8266/spectroscopy-esp8266.ino
@@ -1,31 +1,81 @@
+#include <ESP8266WiFi.h>
 #include "SparkFun_AS7265X.h" 
 AS7265X sensor;
 
 #include <Wire.h>
 
+//const char *ssid = "Spectrometer WIFI Access Point"; // The name of the Wi-Fi network that will be created
+//const char *password = "thecakeisalie";   // The password required to connect to it, leave blank for an open network
+const char *ssid = "TP-Link_E66C"; // The name of the Wi-Fi network that will be created
+const char *password = "Anhsao123";   // The password required to connect to it, leave blank for an open network
+WiFiServer server(80);
+
 float calibrationData[18]; // Creates an array filled with calibration data
+boolean spectrometerLightMeasurement = false; // Setting a toggle for spectroscopy light measurements
 
 void setup() {
   Serial.begin(9600);
-  Serial.read();
-  // Checks if the spectrometer sensor is connected
-  if(sensor.begin() == false) { 
-    Serial.println("Spectrometer sensor is not connected, please check connection and try again");
-    while(1);
+  
+  // Set up WIFI 
+  WiFi.begin(ssid, password);
+  // Makes sure the ESP8266 is connected to WIFI
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println(WiFi.status()); // Prints WL error number 
+    /*
+     * WL codes
+     * WL_NO_SHIELD        = 255   // For compatibility with WiFi Shield library
+     * WL_IDLE_STATUS      = 0
+     * WL_NO_SSID_AVAIL    = 1
+     * WL_SCAN_COMPLETED   = 2
+     * WL_CONNECTED        = 3
+     * WL_CONNECT_FAILED   = 4
+     * WL_CONNECTION_LOST  = 5
+     * WL_DISCONNECTED     = 6
+    */
   }
-  Serial.println("Spectrometer sensor connected");
-  sensor.disableIndicator(); // Turn off the blue status LED to not interfere with readings
+  server.begin(); // Tells the server to begin listening for incoming connections
+  Serial.println("WIFI connected");
+  Serial.print("IP Address: "); 
+  Serial.println(WiFi.localIP()); // Prints IP address
 }
 
 void loop() {
+  checkSpectrometer();
+  checkWIFI();
   getCalibrationData();
-  Serial.println(calibrationData[0]);
-  delay(1000);
 }
 
-void getCalibrationData(){
-  sensor.takeMeasurements();
-  //sensor.takeMeasurementsWithBulb();
+// Checks if the spectrometer is connected
+if(sensor.begin() == false) { 
+    Serial.println("Spectrometer sensor is not connected, please check connection and try again");
+    // Should print error to app
+    while(1);
+} else {
+  sensor.disableIndicator(); // Turn off the blue status LED to not interfere with readings
+}
+
+// Checks if the app is connected to the IP address
+void checkWIFI() {
+  WiFiClient client = server.available();
+  if(client) {
+    String request = client.readStringUntil('\r'); // HTTP request
+    client.flush(); // Waits until all of buffer have been sent
+    
+    // Checks if the suffix of the IP address
+    if(request.indexOf(" /1 ") != -1) {
+      spectrometerLightMeasurement = true;
+      Serial.println("Light measurement on");
+    }
+    else if(request.indexOf(" /2 ") !=  -1) {
+      spectrometerLightMeasurement = false;
+      Serial.println("Light measurement off");
+    }
+  }
+}
+
+// Retrieves the spectrometer data
+void getCalibrationData() {
+  spectrometerLightMeasurement ? sensor.takeMeasurementsWithBulb() : sensor.takeMeasurements(); // Takes measurements based on light toggle
   
   calibrationData[0] = sensor.getCalibratedA();
   calibrationData[1] = sensor.getCalibratedB();
@@ -47,6 +97,4 @@ void getCalibrationData(){
   calibrationData[15] = sensor.getCalibratedU();
   calibrationData[16] = sensor.getCalibratedV();
   calibrationData[17] = sensor.getCalibratedW();
-
-  Serial.println(sensor.getCalibratedA());
 }
